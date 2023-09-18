795e53f021ed5c760ba83b7c0e623a7029283d1f
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-09-18 18:17:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //initial data
 
 let currentQuestion = 0;
+let correctAnswers = 0;
 
 showQuestion();
 
@@ -9,6 +10,9 @@ showQuestion();
 function showQuestion(){
     if(questions[currentQuestion]){
         q = questions[currentQuestion];
+
+        let pct = Math.floor(currentQuestion / questions.length) * 100;
+        document.querySelector('.progress--bar').style.width = `${pct}%`
 
         document.querySelector('.scoreArea').style.display = 'none';
         document.querySelector('.questionArea').style.display = 'block';
@@ -22,7 +26,22 @@ let optionsHtml = '';
             optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span>${q.options[i]}</div>`;            
         }
         document.querySelector('.options').innerHTML = optionsHtml;
+
+        document.querySelectorAll('.options .option').forEach(item => {
+            item.addEventListener('click', optionClickEvent);
+        })
     } else{
         //acabou as questões.
     }
+}
+
+function optionClickEvent(e){
+    let clickOption = parseInt(e.target.getAttribute('data-op'));
+
+    if(questions[currentQuestion].answer === clickOption){
+        correctAnswers++;
+    }
+
+    currentQuestion++;
+    showQuestion()
 }
